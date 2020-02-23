e64b9b826cfa804808dded91fe91562e4dd977cd
Bhaskar <Bhas957@gmail.com>
2020-02-23 17:24:19 +0530

    Added diagram functions

--- src/test/java/com/example/demo/Shapes.java
+++ src/test/java/com/example/demo/Shapes.java
@@ -1,11 +1,39 @@
 package com.example.demo;
 
-
 public class Shapes {
     public static void main(String[] args) {
-
-        System.out.println("bhaskar");
+        diamondStarPattern(3);
     }
 
+    /*
+    input 3
+    Output      *
+               * *
+              * * *
+               * *
+                *
+     */
+    public static void diamondStarPattern(int num) {
+        int i = findLengthofPattern(num);
+        for (int k = 1; k <= i; k++) {
+            if (k > num) System.out.println(printShape(k - num, i, "*"));
+            else System.out.println(printShape(k, i, "*"));
+        }
+    }
 
+    public static String printShape(int number, int maxLength, String patternshape) {
+        String pattern = "";
+        for (int i = 1; i <= number; i++) {
+            pattern = " " + patternshape + " ";
+        }
+        int adjustLen = (maxLength - pattern.length()) / 2;
+        for (int i = 0; i < adjustLen; i++) pattern = " " + patternshape + " ";
+
+        System.out.println(pattern);
+        return pattern;
+    }
+
+    public static int findLengthofPattern(int num) {
+        return 2 * num + 1;
+    }
 }
